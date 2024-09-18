eb25f932c019acd692ebef537c365487ef18368a
FidelAraguzLara <fidel.lara.araguz@gmail.com>
2024-09-17 23:23:17 -0600

    Test: Se completaron las pruebas para player y Game

--- test/Game.test.js
+++ test/Game.test.js
@@ -1,3 +1,6 @@
+import { Game } from "../src/models/Game";
+import { Player } from "../src/models/Player";
+
 test("Al crear el objeto la longitud de players es 0", () => {
   const game = new Game();
   expect(game.getPlayers().length).toBe(0);
@@ -6,6 +9,6 @@ test("Al crear el objeto la longitud de players es 0", () => {
 
 test("Al añadir los jugadores la cantidad debe ser cuatro", () => {
     const game = new Game();
-    game.setPlayers();
-    expect(game.getPlayers().length).toBe(0);
+    game.setPlayers([new Player(), new Player(), new Player(), new Player()]);
+    expect(game.getPlayers().length).toBe(4);
   });
